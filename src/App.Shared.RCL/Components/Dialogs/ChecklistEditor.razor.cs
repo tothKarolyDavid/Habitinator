ec7b4c35cbe9556a30751ec98be1d47c1c1c9f11
Károly Dávid Tóth <kadatodev@gmail.com>
2026-09-02 10:30:54 +0200

    refactor(dialogs): merge ChecklistEditor into single Razor file

--- src/App.Shared.RCL/Components/Dialogs/ChecklistEditor.razor.cs
+++
@@ -1,8 +0,0 @@
-using Microsoft.AspNetCore.Components;
-
-namespace App.Shared.RCL.Components.Dialogs;
-
-public partial class ChecklistEditor
-{
-    [Parameter] public EventCallback<List<ChecklistRow>> ValueChanged { get; set; }
-}
